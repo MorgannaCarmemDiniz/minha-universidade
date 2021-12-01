290828e8b58527b0ba8e5c560beb2dcbe6619a6d
João Vitor Sousa <joao.sousa@edu.unirio.br>
2021-12-01 17:19:59 -0300

    Organização do plugin do fontawesome

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
+import Vue from 'vue';
 import '@babel/polyfill';
 import 'mutationobserver-shim';
-import Vue from 'vue';
 import './plugins/bootstrap-vue';
+import './plugins/fontawesome';
 import App from './App.vue';
 import router from './router';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {faEnvelope, faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import VueAnimateOnScroll from 'vue-animate-onscroll'
-
 import './css/main.scss';
 
 Vue.config.productionTip = false;
 
-library.add(faExternalLinkAlt);
-library.add(faEnvelope);
-Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.use(VueAnimateOnScroll);
 
 new Vue({
