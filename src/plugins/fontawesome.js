a7e8d31dc3e7c5f303a2dca828cb03a5fc260e18
João Vitor Sousa <joao.sousa@edu.unirio.br>
2021-12-18 22:40:01 -0300

    Adição de um ícone do fontawesome.

--- src/plugins/fontawesome.js
+++ src/plugins/fontawesome.js
@@ -1,8 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {faEnvelope, faExternalLinkAlt} from '@fortawesome/free-solid-svg-icons';
+import {faEnvelope, faExternalLinkAlt, faBars} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue from "vue";
 
 library.add(faExternalLinkAlt);
 library.add(faEnvelope);
+library.add(faBars);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
